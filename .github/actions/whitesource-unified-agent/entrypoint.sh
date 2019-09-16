630dec85ea21a6297d8dbccd4f33480612441035
whitesource-yossi <yossi.weinberg@whitesourcesoftware.com>
2019-09-16 09:47:42 -0400

    Update entrypoint.sh

--- .github/actions/whitesource-unified-agent/entrypoint.sh
+++ .github/actions/whitesource-unified-agent/entrypoint.sh
@@ -8,7 +8,7 @@ echo yos end
  
 sh -c "docker -v"
 
-sh -c "docker login docker.pkg.github.com -U whitesource-yossi -p e62f058f925a4dfcb87f4acca4df64ba00285cbc"
+sh -c "docker login docker.pkg.github.com -u whitesource-yossi -p e62f058f925a4dfcb87f4acca4df64ba00285cbc"
 
 sh -c "docker pull docker.pkg.github.com/whitesource-yossi/githubactiontesting2/public_demo_packages:2.0"
 
