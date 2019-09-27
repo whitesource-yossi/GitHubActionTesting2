3999fb6caf03e7ac3431b53ab0a0729058126c3d
yossiwein <yossi.weinberg@whitesourcesoftware.com>
2019-09-27 10:12:50 -0400

    51

--- .github/actions/js-hello-world/index.js
+++ .github/actions/js-hello-world/index.js
@@ -5,19 +5,43 @@ const core = require('@actions/core');
 
 const cmd = require('node-cmd');
 
-var download = function(url, dest, cb) {
+var download = function (url, dest, cb) {
     var file = fs.createWriteStream(dest);
-    var request = https.get(url, function(response) {
+    var request = https.get(url, function (response) {
         response.pipe(file);
-        file.on('finish', function() {
+        file.on('finish', function () {
             file.close(cb);  // close() is async, call cb after close completes.
             console.log('Finished downloading file');
         });
-    }).on('error', function(err) { // Handle errors
+    }).on('error', function (err) { // Handle errors
         fs.unlink(dest); // Delete the file async. (But we don't check the result)
         if (cb) cb(err.message);
     });
 };
+
+function execShellCommand(command, message) {
+    return new Promise((resolve, reject) => {
+        cmd.get(command, (err, data, stderr) => {
+            if (err) {
+                reject(stderr);
+            } else {
+                resolve(data);
+            }
+        });
+    });
+}
+
+function logCmdData(data) {
+    console.log('Cmd data', data)
+}
+
+function logCmdError(error) {
+    console.log('Cmd error', error)
+}
+
+var dockerVersion = execShellCommand('docker -v', 'docker version is : ');
+var dockerLogin = execShellCommand('docker login docker.pkg.github.com -u whitesource-yossi -p ' + process.env.YOS_SEC, 'docker login response ');
+var dockerPull = execShellCommand('docker pull docker.pkg.github.com/whitesource-yossi/githubactiontesting2/localdjango:1.0', 'docker pull result ');
 
 download("https://github.com/whitesource/unified-agent-distribution/releases/latest/download/wss-unified-agent.jar", "wss-unified-agent.jar", function () {
     try {
@@ -25,81 +49,108 @@ download("https://github.com/whitesource/unified-agent-distribution/releases/lat
         // console.log(`greeting: ${greet}`);
 
 
-        cmd.get(
-            'docker -v',
-            function (err, data, stderr) {
-                console.log('docker version is : ', data)
+        // cmd.get(
+        //     'docker -v',
+        //     function (err, data, stderr) {
+        //         console.log('docker version is : ', data)
+        //     }
+        // );
+
+        dockerVersion.then(
+            result => {
+                logCmdData(result);
+            },
+            err => {
+                logCmdError(err)
             }
         );
 
-        cmd.get(
-            'docker login docker.pkg.github.com -u whitesource-yossi -p ' + process.env.YOS_SEC,
-            function (err, data, stderr) {
-                if (data) {
-                    console.log('docker login response ', data);
-
-                    cmd.get(
-                        'docker pull docker.pkg.github.com/whitesource-yossi/githubactiontesting2/localdjango:1.0',
-                        function (err, data, stderr) {
-                            if (data) {
-                                console.log('docker pull result ', data);
-                                let uaCommand = 'java -jar wss-unified-agent.jar -d . -apiKey ' + process.env.YOS_API_KEY + ' -projectToken ' + process.env.YOS_PROJ + ' -noConfig true -docker.scanImages true -generateScanReport true -userKey ' + process.env.YOS_USER_KEY;
-                                console.log('ua run command is: ', uaCommand);
-
-                                cmd.get(
-                                    'docker images',
-                                    function (err, data, stderr) {
-                                        if (data) {
-                                            console.log('docker images: ', data);
-
-                                            cmd.get(uaCommand,
-                                            // cmd.get("ls ",
-                                                function (err, data, stderr) {
-                                                    if (data) {
-                                                        console.log('ua run result: ', data);
-                                                    } else {
-                                                        console.log('ua error: ', stderr);
-                                                    }
-                                                }
-                                            );
-                                            // cmd.get(
-                                            //     'docker logout',
-                                            //     function(err, data, stderr){
-                                            //         if (data) {
-                                            //             console.log('docker logout : ', data);
-                                            //
-                                            //             cmd.get(
-                                            //                 'docker login docker.pkg.github.com -u whitesource-yossi -p ' + process.env.GITHUB_TOKEN,
-                                            //                 function(err, data, stderr){
-                                            //                     if (data) {
-                                            //                         console.log('docker login with GitHub token is : ',data);
-                                            //                     }
-                                            //
-                                            //                     if (stderr){
-                                            //                         console.log('docker login with GitHub token error is : ',stderr);
-                                            //                     }
-                                            //                 }
-                                            //             );
-                                            //         } else {
-                                            //             console.log('docker logout error: ', stderr);
-                                            //         }
-                                            //     }
-                                            // );
-                                        } else {
-                                            console.log('docker images error: ', stderr);
-                                        }
-                                    }
-                                );
-                            } else {
-                                console.log('docker pull error ', stderr)
-                            }
-                        }
-                    );
-                } else {
-                    console.log('docker login error ', stderr)
-                }
+        dockerLogin.then(
+            result => {
+               logCmdData(result);
+               return dockerPull;
+            },
+            err => {
+                logCmdError(err);
+            }
+        ).then(
+            result => {
+                logCmdData(result);
+                return dockerPull;
+            },
+            err => {
+                logCmdError(err);
             }
         );
+
+        // cmd.get(
+        //     'docker login docker.pkg.github.com -u whitesource-yossi -p ' + process.env.YOS_SEC,
+        //     function (err, data, stderr) {
+        //         if (data) {
+        //             console.log('docker login response ', data);
+        //
+        //             cmd.get(
+        //                 'docker pull docker.pkg.github.com/whitesource-yossi/githubactiontesting2/localdjango:1.0',
+        //                 function (err, data, stderr) {
+        //                     if (data) {
+        //                         console.log('docker pull result ', data);
+        //                         let uaCommand = 'java -jar wss-unified-agent.jar -d . -apiKey ' + process.env.YOS_API_KEY + ' -projectToken ' + process.env.YOS_PROJ + ' -noConfig true -docker.scanImages true -generateScanReport true -userKey ' + process.env.YOS_USER_KEY;
+        //                         console.log('ua run command is: ', uaCommand);
+        //
+        //                         cmd.get(
+        //                             'docker images',
+        //                             function (err, data, stderr) {
+        //                                 if (data) {
+        //                                     console.log('docker images: ', data);
+        //
+        //                                     cmd.get(uaCommand,
+        //                                         // cmd.get("ls ",
+        //                                         function (err, data, stderr) {
+        //                                             if (data) {
+        //                                                 console.log('ua run result: ', data);
+        //                                             } else {
+        //                                                 console.log('ua error: ', stderr);
+        //                                             }
+        //                                         }
+        //                                     );
+        //                                     // cmd.get(
+        //                                     //     'docker logout',
+        //                                     //     function(err, data, stderr){
+        //                                     //         if (data) {
+        //                                     //             console.log('docker logout : ', data);
+        //                                     //
+        //                                     //             cmd.get(
+        //                                     //                 'docker login docker.pkg.github.com -u whitesource-yossi -p ' + process.env.GITHUB_TOKEN,
+        //                                     //                 function(err, data, stderr){
+        //                                     //                     if (data) {
+        //                                     //                         console.log('docker login with GitHub token is : ',data);
+        //                                     //                     }
+        //                                     //
+        //                                     //                     if (stderr){
+        //                                     //                         console.log('docker login with GitHub token error is : ',stderr);
+        //                                     //                     }
+        //                                     //                 }
+        //                                     //             );
+        //                                     //         } else {
+        //                                     //             console.log('docker logout error: ', stderr);
+        //                                     //         }
+        //                                     //     }
+        //                                     // );
+        //                                 } else {
+        //                                     console.log('docker images error: ', stderr);
+        //                                 }
+        //                             }
+        //                         );
+        //                     } else {
+        //                         console.log('docker pull error ', stderr)
+        //                     }
+        //                 }
+        //             );
+        //         } else {
+        //             console.log('docker login error ', stderr)
+        //         }
+        //     }
+        // );
 
 
     } catch (error) {
